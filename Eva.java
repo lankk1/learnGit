de21be241f7938d777f4d95f65279c49fe426810
lankk1 <835264673@qq.com>
2017-10-27 15:54:37 +0800

    哈哈哈
    Signed-off-by: lankk1 <835

---
+++ Eva.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Created by KJA on 2017/2/27.
+ */
+public class Eva {
+    private Integer productId;
+    private String context;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+}
