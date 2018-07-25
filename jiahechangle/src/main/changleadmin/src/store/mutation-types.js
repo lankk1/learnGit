8771ca68ddc2ae7318edfb728884ef445378016f
lankk1 <835264673@qq.com>
2018-07-25 15:28:43 +0800

    delete

--- jiahechangle/src/main/changleadmin/src/store/mutation-types.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by slipkinem on 2016/12/9.
- */
-'use strict'
-export const GET_TABLEDATA = 'GET_TABLEDATA'
-export const GET_MESSAGEDATA = 'GET_MESSAGEDATA'
-export const GET_NEWS = 'GET_NEWS'
